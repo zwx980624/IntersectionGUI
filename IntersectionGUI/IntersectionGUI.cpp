2205e44bb4677d61a626807ddfd7cf2c737c82c5
zwx <853845818@qq.com>
2020-03-17 20:57:36 +0800

    all draw_shapes; add dll; calcuIntersection ok; show all points and lines and axis ok;

--- IntersectionGUI/IntersectionGUI.cpp
+++ IntersectionGUI/IntersectionGUI.cpp
@@ -7,6 +7,13 @@
 #include <QDir>
 #include <QFileDialog>
 #include <QPainter>
+#include <vector>
+#include <utility>
+#include <QMessageBox>
+
+#pragma comment(lib,"calcInterface.lib")
+_declspec(dllexport) extern "C" int guiProcess(std::vector<std::pair<double, double>> *points, std::string msg);
+_declspec(dllexport) extern "C" void cmdProcess(int argc, char *argv[]);
 
 IntersectionGUI::IntersectionGUI(QWidget *parent)
 	: QMainWindow(parent)
@@ -33,7 +40,7 @@ void IntersectionGUI::on_actAddShape_triggered()
 	QString str;
 	if (shapeType == QString::fromLocal8Bit("直线")) {
 		str = str.sprintf("L %d %d %d %d",data1, data2, data3, data4);
-		draw_seg(data1, data2, data3, data4);
+		draw_line(data1, data2, data3, data4);
 	}
 	else if (shapeType == QString::fromLocal8Bit("线段")) {
 		str = str.sprintf("S %d %d %d %d", data1, data2, data3, data4);
@@ -41,7 +48,7 @@ void IntersectionGUI::on_actAddShape_triggered()
 	}
 	else if (shapeType == QString::fromLocal8Bit("射线")) {
 		str = str.sprintf("R %d %d %d %d", data1, data2, data3, data4);
-		draw_seg(data1, data2, data3, data4);
+		draw_ray(data1, data2, data3, data4);
 	}
 	else {
 		str =  str.sprintf("C %d %d %d", data1, data2, data3);
@@ -97,6 +104,11 @@ void IntersectionGUI::on_actAddFile_triggered() {
 		while (N--) {
 			std::getline(fin, line);
 			QString qline = QString::fromStdString(line);
+			if (!isShapeStrValid(qline)) {
+				QString dlgTitle = QString::fromLocal8Bit("输入格式错误");
+				QMessageBox::critical(this, dlgTitle, qline);
+				break;
+			}
 			draw_shape_from_str(qline);
 			QListWidgetItem * aItem = new QListWidgetItem(); //新建一个项
 			aItem->setText(qline); //设置文字标签
@@ -107,19 +119,45 @@ void IntersectionGUI::on_actAddFile_triggered() {
 	}
 }
 
+void IntersectionGUI::on_actSolve_triggered()
+{
+	std::string input;
+	input += std::to_string(ui.listWidget->count()) + "\n";
+	for (int i = 0; i < ui.listWidget->count(); i++)
+	{
+		QListWidgetItem *aItem = ui.listWidget->item(i);//获取一个项
+		QString str = aItem->text();
+		input += str.toStdString() + "\n";
+	}
+	std::vector<std::pair<double, double> > points;
+	int cnt = guiProcess(&points, input);
+	QString str = str.asprintf("%d %d %d", cnt, (int)(points.begin()->first), (int)(points.begin()->second));
+	LabStatus->setText(str);
+	for (auto vit = points.begin(); vit != points.end(); ++vit) {
+		int w, h;
+		xy2wh((int)(vit->first), (int)(vit->second), w, h);
+		QString str = str.asprintf("%d %d %d", cnt, w, h);
+		LabStatus->setText(str);
+		draw_point(w, h, Qt::red);
+	}
+}
+
 void IntersectionGUI::draw_point(int const x, int const y, QColor const c, int const w) {
 	// 在QPixmap上画图
 	QPainter Painter(&curPixmap);
+	Painter.setRenderHint(QPainter::Antialiasing, true);
 
 	Painter.setPen(QPen(c, w));
-
-	Painter.drawPoint(x, y);
+	QBrush brush(c);
+	Painter.setBrush(brush);
+	Painter.drawEllipse(x - w, y - w , w * 2, w * 2);
+	ui.canvas->setPixmap(curPixmap);
 }
 
 void IntersectionGUI::draw_axis()
 {
 	QPainter Painter(&curPixmap);
-	Painter.setPen(QPen(Qt::black, 2));
+	Painter.setPen(QPen(Qt::green, 2));
 	Painter.drawLine(QPoint(0, homeH), QPoint(CANVAS_SIZE, homeH));
 	Painter.drawLine(QPoint(homeW, CANVAS_SIZE), QPoint(homeW , 0));
 }
@@ -134,17 +172,77 @@ void IntersectionGUI::init_canvas()
 
 void IntersectionGUI::draw_line(int x1, int y1, int x2, int y2, QColor const c, int const w)
 {
-	
-	QPainter Painter(&curPixmap);
-	Painter.setPen(QPen(c, w));
-	Painter.drawLine(QPoint(0, homeH), QPoint(CANVAS_SIZE, homeH));
-	Painter.drawLine(QPoint(homeW, CANVAS_SIZE), QPoint(homeW, 0));
+	IntersectionGUI::draw_ray(x1, y1, x2, y2, c, w);
+	IntersectionGUI::draw_ray(x2, y2, x1, y1, c, w);
 }
 
 void IntersectionGUI::draw_seg(int x1, int y1, int x2, int y2, QColor const c, int const w)
 {
 	QPainter Painter(&curPixmap);
+	Painter.setRenderHint(QPainter::Antialiasing, true);
 	Painter.setPen(QPen(c, w));
+	QPoint p1 = xy2whPoint(x1, y1);
+	QPoint p2 = xy2whPoint(x2, y2);
+	QString str = str.asprintf("%d %d %d %d %d %d %d %d", p1.x(), p1.y(), p2.x(), p2.y(), x1, y1, x2, y2);
+	LabStatus->setText(str);
+	Painter.drawLine(p1, p2);
+	ui.canvas->setPixmap(curPixmap);
+}
+
+void IntersectionGUI::draw_ray(int x1, int y1, int x2, int y2, QColor const c, int const w) {
+	QPainter Painter(&curPixmap);
+	Painter.setRenderHint(QPainter::Antialiasing, true);
+	Painter.setPen(QPen(c, w));
+	if (x2 == x1) {
+		if (y2 > y1) {
+			y2 = REAL_SIZE;
+		}
+		else {
+			y2 = -REAL_SIZE;
+		}
+	}
+	else if (y1 == y2) {
+		if (x2 > x1) {
+			x2 = REAL_SIZE;
+		}
+		else {
+			x2 = -REAL_SIZE;
+		}
+	}
+	else {
+		double k = (double)(y2 - y1) / (x2 - x1);
+		double b = y1 - k * x1;
+		if (x2 > x1) {
+			double y_ = REAL_SIZE * k + b;
+			if (y_ > REAL_SIZE) {
+				y2 = REAL_SIZE;
+				x2 = (y2 - b) / k;
+			}
+			else if (y_ < -REAL_SIZE) {
+				y2 = -REAL_SIZE;
+				x2 = (y2 - b) / k;
+			}
+			else {
+				x2 = REAL_SIZE;
+				y2 = y_;
+			}
+		}
+		else {
+			double y_ = -REAL_SIZE * k + b;
+			if (y_ > REAL_SIZE) {
+				y2 = REAL_SIZE;
+				x2 = (y2 - b) / k;
+			}
+			else if (y_ < -REAL_SIZE) {
+				y2 = -REAL_SIZE;
+				x2 = (y2 - b) / k;
+			}
+			else {
+				x2 = -REAL_SIZE;
+				y2 = y_;
+			}
+		}
+	}
 	QPoint p1 = xy2whPoint(x1, y1);
 	QPoint p2 = xy2whPoint(x2, y2);
 	QString str = str.asprintf("%d %d %d %d %d %d %d %d", p1.x(), p1.y(), p2.x(), p2.y(), x1, y1, x2, y2);
@@ -156,6 +254,7 @@ void IntersectionGUI::draw_seg(int x1, int y1, int x2, int y2, QColor const c, i
 void IntersectionGUI::draw_circle(int x0, int y0, int r, QColor const c, int const w)
 {
 	QPainter Painter(&curPixmap);
+	Painter.setRenderHint(QPainter::Antialiasing, true);
 	Painter.setPen(QPen(c, w));
 	QPoint p0 = xy2whPoint(x0, y0);
 	int r0 = double(r)* RATIO;
@@ -185,7 +284,7 @@ void IntersectionGUI::draw_shape_from_str(QString str) {
 		int data2 = str.section(" ", 2, 2).toInt();
 		int data3 = str.section(" ", 3, 3).toInt();
 		int data4 = str.section(" ", 4, 4).toInt();
-		draw_seg(data1, data2, data3, data4);
+		draw_line(data1, data2, data3, data4);
 	}
 	else if (shapeType == QString::fromLocal8Bit("S")) {
 		int data1 = str.section(" ", 1, 1).toInt();
@@ -199,7 +298,7 @@ void IntersectionGUI::draw_shape_from_str(QString str) {
 		int data2 = str.section(" ", 2, 2).toInt();
 		int data3 = str.section(" ", 3, 3).toInt();
 		int data4 = str.section(" ", 4, 4).toInt();
-		draw_seg(data1, data2, data3, data4);
+		draw_ray(data1, data2, data3, data4);
 	}
 	else {
 		int data1 = str.section(" ", 1, 1).toInt();
@@ -207,5 +306,15 @@ void IntersectionGUI::draw_shape_from_str(QString str) {
 		int data3 = str.section(" ", 3, 3).toInt();
 		draw_circle(data1, data2, data3);
 	}
+}
+
+bool IntersectionGUI::isShapeStrValid(QString str)
+{
+	str = str.trimmed();
+	QRegExp re("([LRS]( (-|\\+)?\\d+){4})|(C( (-|\\+)?\\d+){3})");
+	if (!re.exactMatch(str)) {
+		return false;
+	}
+	return true;
 }
 
--- IntersectionGUI/IntersectionGUI.h
+++ IntersectionGUI/IntersectionGUI.h
@@ -20,16 +20,18 @@ private:
 	Ui::IntersectionGUIClass ui;
 	QString lastPath;
 	QPixmap curPixmap; //µ±Ç°µÄÍ¼Æ¬
-	void draw_point(int const x, int const y, QColor const c, int const w);
+	void draw_point(int const x, int const y, QColor const c, int const w = 4);
 	int homeW = CANVAS_SIZE / 2;
 	int homeH = CANVAS_SIZE / 2;
 	void draw_axis();
 	void init_canvas();
-	void draw_line(int x1, int y1, int x2, int y2, QColor const c, int const w);
+	void draw_line(int x1, int y1, int x2, int y2, QColor const c = Qt::black, int const w = 2);
 	void draw_seg(int x1, int y1, int x2, int y2, QColor const c = Qt::black, int const w = 2);
+	void draw_ray(int x1, int y1, int x2, int y2, QColor const c = Qt::black, int const w = 2);
 	void draw_circle(int x0, int y0, int r, QColor const c = Qt::black, int const w = 2);
 	void xy2wh(int x, int y, int& w, int& h);
 	void draw_shape_from_str(QString str);
+	bool isShapeStrValid(QString str);
 	QPoint xy2whPoint(int x, int y);
 	QLabel *LabStatus;
 private slots:
@@ -37,6 +39,7 @@ private slots:
 	void on_actDelShape_triggered();
 	void on_actClear_triggered();
 	void on_actAddFile_triggered();
+	void on_actSolve_triggered();
 };
 
 
